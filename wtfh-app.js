92a8e3b07d9b3975f911e087a8c3b6e1d40e80dd
Bowie W Sessions <32888862+bsessio@users.noreply.github.com>
2019-05-05 16:02:42 -0500

    Update wtfh-app.js

--- wtfh-app.js
+++ wtfh-app.js
@@ -1,7 +1,10 @@
 let navs = document.querySelectorAll('.nav li');
 let contents = document.querySelectorAll('.tab');
  
-toggler = (excl, allEls) => {
+function myFunction(x) {
+ if(window.matchMedia("(max-width: 1366px)")
+    {
+ toggler = (excl, allEls) => {
   allEls.forEach((v, k) => {
     if (excl != k) {
       v.classList.remove('active');
@@ -17,7 +20,8 @@ toggler = (excl, allEls) => {
       toggler(k, contents);
     });
   });
-  
+}
+else {
 !function(t){"use strict";function e(t,e){return function(o){o.target&&o.target.matches(t)&&e.apply(this,arguments)}}Element.prototype.matches||(Element.prototype.matches=Element.prototype.matchesSelector||Element.prototype.mozMatchesSelector||Element.prototype.msMatchesSelector||Element.prototype.oMatchesSelector||Element.prototype.webkitMatchesSelector||function(t){for(var e=(this.document||this.ownerDocument).querySelectorAll(t),o=e.length;--o>=0&&e.item(o)!==this;);return o>-1}),t.addDynamicEventListener=function(t,o,n,r,c){t.addEventListener(o,e(n,r),c)}}(this);
 
 addDynamicEventListener(document.body, 'click', '.nav, li', function (e) {
@@ -41,3 +45,4 @@ addDynamicEventListener(document.body, 'click', '.nav, li', function (e) {
     });
   });
 });
+}

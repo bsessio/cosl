7ca9e2c9eaec4b3359d353ac3c6241545dab909d
Bowie W Sessions <32888862+bsessio@users.noreply.github.com>
2019-05-05 17:09:23 -0500

    Update wtfh-app.js

--- wtfh-app.js
+++ wtfh-app.js
@@ -1,6 +1,18 @@
 let navs = document.querySelectorAll('.nav li');
 let contents = document.querySelectorAll('.tab');
 let x = window.matchMedia("(max-width: 1366px)")
+!function(t){"use strict";
+             function e(t,e){
+              return function(o){
+               o.target&&o.target.matches(t)&&e.apply(this,arguments)
+              }}
+             Element.prototype.matches||(Element.prototype.matches=Element.prototype.matchesSelector||Element.prototype.mozMatchesSelector||Element.prototype.msMatchesSelector||Element.prototype.oMatchesSelector||Element.prototype.webkitMatchesSelector||function(t){for(var e=(this.document||this.ownerDocument).querySelectorAll(t),o=e.length;--o>=0&&e.item(o)!==this;);
+             return o>-1
+             }),
+              t.addDynamicEventListener=function(t,o,n,r,c){
+              t.addEventListener(o,e(n,r),c)
+             }}
+            (this);
 
 function myFunction(x) {
  if(x.matches)
@@ -23,8 +35,6 @@ function myFunction(x) {
   });
 }
 else {
-!function(t){"use strict";function e(t,e){return function(o){o.target&&o.target.matches(t)&&e.apply(this,arguments)}}Element.prototype.matches||(Element.prototype.matches=Element.prototype.matchesSelector||Element.prototype.mozMatchesSelector||Element.prototype.msMatchesSelector||Element.prototype.oMatchesSelector||Element.prototype.webkitMatchesSelector||function(t){for(var e=(this.document||this.ownerDocument).querySelectorAll(t),o=e.length;--o>=0&&e.item(o)!==this;);return o>-1}),t.addDynamicEventListener=function(t,o,n,r,c){t.addEventListener(o,e(n,r),c)}}(this);
-
 addDynamicEventListener(document.body, 'click', '.nav, li', function (e) {
   let navs = document.querySelectorAll('.nav li');
   let contents = document.querySelectorAll('.tab');

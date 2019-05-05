97663d9aa437995484d0babfaf0299f1ff66f377
Bowie W Sessions <32888862+bsessio@users.noreply.github.com>
2019-05-05 00:02:50 -0500

    Update wtfh-app.js

--- wtfh-app.js
+++ wtfh-app.js
@@ -1,21 +1,3 @@
 document.addEventListener('DOMContentLoaded', function() {
-  let navs = document.querySelectorAll('.nav li');
-  let contents = document.querySelectorAll('.tab');
-
-  toggler = (excl, allEls) => {
-  allEls.forEach((v, k) => {
-    if (excl != k) {
-      v.classList.remove('active');
-    }
-  });
-  }
-
-  navs.forEach((v, k) => {
-    v.addEventListener('click', (e, i) => {
-      v.classList.add('active');
-      toggler(k, navs);
-      contents[k].classList.add('active');
-      toggler(k, contents);
-    });
-  });
+  console.log("Hello");
 });

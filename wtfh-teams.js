62e01002c4259d67589f27859fb2c6040867f025
Bowie W Sessions <32888862+bsessio@users.noreply.github.com>
2019-05-06 11:50:59 -0500

    Update wtfh-teams.js

--- wtfh-teams.js
+++ wtfh-teams.js
@@ -2,9 +2,14 @@ function reveal(ident) {
     revealing=document.getElementById(ident).classList;
     if(revealing[0] == 'hidden') revealing.remove("hidden");
     else revealing.add("hidden");
-}
+};
 
 document.addEventListener("click", (e) => {
     let ident = e.path[0].attributes[0].value;
     if (ident) reveal(ident)
-  })
+  });
+
+document.addEventListener("touchstart", (e) => {
+    let ident = e.path[0].attributes[0].value;
+    if (ident) reveal(ident)
+  });

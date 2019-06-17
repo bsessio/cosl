71a84b68db2370ac648ce9b31fe86f4c392813ee
Bowie W Sessions <32888862+bsessio@users.noreply.github.com>
2019-06-17 14:49:22 -0500

    Add files via upload

---
+++ wtfh-plot.js
@@ -0,0 +1,81 @@
+let appNavs = document.querySelectorAll('.nav li');
+let appContents = document.querySelectorAll('.tab');
+let appX = window.matchMedia("(max-width: 1366px)")
+
+!function(t){"use strict";
+             function e(t,e){
+              return function(o){
+               o.target&&o.target.matches(t)&&e.apply(this,arguments)
+              }}
+             Element.prototype.matches||(Element.prototype.matches=Element.prototype.matchesSelector||Element.prototype.mozMatchesSelector||Element.prototype.msMatchesSelector||Element.prototype.oMatchesSelector||Element.prototype.webkitMatchesSelector||function(t){for(var e=(this.document||this.ownerDocument).querySelectorAll(t),o=e.length;--o>=0&&e.item(o)!==this;);
+             return o>-1
+             }),
+              t.addDynamicEventListener=function(t,o,n,r,c){
+              t.addEventListener(o,e(n,r),c)
+             }}
+            (this);
+
+function reveal(ident) {
+	revealing=document.getElementById(ident).classList;
+	if(revealing[0] == 'hidden') revealing.remove("hidden");
+	else revealing.add("hidden");
+};
+
+document.addEventListener("click", (e, err) => {
+	let ident = e.path[0].attributes[0].value;
+	if (ident) reveal(ident)
+	});
+
+
+document.addEventListener("touchstart", (e, err) => {
+	let ident = e.target.dataset.attr;
+	if (ident) reveal(ident)
+	});
+
+function appMediaQuery(appX) {
+ if(appX.matches)
+    {
+ appToggler = (excl, allEls) => {
+  allEls.forEach((v, k) => {
+    if (excl != k) {
+      v.classList.remove('active');
+    }
+  });
+  }
+
+  appNavs.forEach((v, k) => {
+    v.addEventListener('click', (e, i) => {
+      v.classList.add('active');
+      appToggler(k, appNavs);
+      appContents[k].classList.add('active');
+      appToggler(k, appContents);
+    });
+  });
+}
+else {
+addDynamicEventListener(document.body, 'click', '.nav, li', function (e) {
+  let appNavs = document.querySelectorAll('.nav li');
+  let appContents = document.querySelectorAll('.tab');
+
+  appToggler = (excl, allEls) => {
+  allEls.forEach((v, k) => {
+    if (excl != k) {
+      v.classList.remove('active');
+    }
+  });
+  }
+
+  appNavs.forEach((v, k) => {
+    v.addEventListener('click', (e, i) => {
+      v.classList.add('active');
+      appToggler(k, appNavs);
+      appContents[k].classList.add('active');
+      appToggler(k, appContents);
+    });
+  });
+}); 
+}
+}
+
+appMediaQuery(appX) // Call listener function at run time
+appX.addListener(appMediaQuery) // Attach listener function on state changes
